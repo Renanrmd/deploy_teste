a68139edd509f0ec84775b9a577c276844383685
Renan Martins <renanmartinsdutra@gmail.com>
2020-12-16 15:35:13 -0300

    alterando nome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Harmonize
+                    Harmonize compilando
                 </div>
 
                 <div class="links">
