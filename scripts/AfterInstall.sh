307705e0def0c080579548ab30e86469b27881ae
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-14 17:04:40 -0300

    adicionando novos comandos

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,3 +2,6 @@
 sudo chown -R www-data.www.data /home/harmonize/deploy_teste/storage
 sudo chown -R www-data.www.data /home/harmonize/deploy_teste/bootstrap/cache
 cd /home/harmonize/deploy_teste
+composer install
+npm install
+npm run prod
