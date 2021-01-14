a2945a4e63339e3814d842f03060cabd3e47ecaf
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-14 17:46:53 -0300

    adicionando reescrever no arquivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Harmonize
+                    Harmonize - App
                 </div>
 
                 <div class="links">
