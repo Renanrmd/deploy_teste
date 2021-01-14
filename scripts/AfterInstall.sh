d3a518be4c4ff81f9e32cd83e956fd3c10bf168b
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-14 16:24:59 -0300

    testando alterações

---
+++ scripts/AfterInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo chown -R www-data.www.data /home/harmonize/deploy_teste/storage
+sudo chown -R www-data.www.data /home/harmonize/deploy_teste/bootstrap/cache
+cd /home/harmonize/deploy_teste
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm -rf /home/harmonize/deploy_teste/*
