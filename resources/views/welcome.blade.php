71a92fb4bace77f0992c97925cd46c64e54a98e9
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-15 18:17:16 -0300

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Harmonize
+                    Harmonize - Online
                 </div>
 
                 <div class="links">
