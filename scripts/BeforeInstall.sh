eff98f30bf7a9ec7acf38c7917f83b8e1d5d2664
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-15 09:16:00 -0300

    desfazendo alteração

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws deploy get-application --application-name deploy_teste --file-exists-behavior=OVERWRITE
-# sudo rm -rf /home/harmonize/deploy_teste/*
+# aws deploy get-application --application-name deploy_teste --file-exists-behavior=OVERWRITE
+sudo rm -rf /home/harmonize/deploy_teste/*
