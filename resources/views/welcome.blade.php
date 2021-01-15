c4da1d72b78742edceba0a863cc61572d4a7ed71
Renan Martins <renanmartinsdutra@gmail.com>
2021-01-15 10:51:17 -0300

    testando aws

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Harmonize - App online
+                    Oi
                 </div>
 
                 <div class="links">
